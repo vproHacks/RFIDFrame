ca6b2217b1ce1bbedd44c2729c258e6894a262ce
VPRO <blahthing8@gmail.com>
2019-06-14 12:46:48 -0400

    Did some work with pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='RFIDFrame',
+     version='0.01',
+     scripts=['RFIDFrame'] ,
+     author="Vraj Prajapati",
+     author_email="blahthing8@gmail.com",
+     description="A Framework to use RFID Devicec",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/vproHacks/RFIDFrame",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: None",
+         "Operating System :: OS Independent",
+     ],
+ )
